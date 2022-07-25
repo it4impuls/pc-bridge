2bf7a36b43ab08c804713e2f80abda581f28ea91
marilynias <98028585+marilynias@users.noreply.github.com>
2022-07-25 14:37:37 +0200

    setup.sh allowed hosts

--- PC_bridge/PC_bridge/settings.py
+++ PC_bridge/PC_bridge/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-b)l!sep&3i82115z3&#$160@o(smm8x9z+m7%@^xjw+(onu+$q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.100.119']
 
 
 # Application definition
--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 pip install -r req.txt
-python3 PC_bridge/manage.py runserver
+python3 PC_bridge/manage.py migrate
+python3 PC_bridge/manage.py runserver 0.0.0.0:8000
