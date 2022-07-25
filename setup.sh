a2e3173246635ba10ef40dd2532e8fcfcc46f575
marilynias <98028585+marilynias@users.noreply.github.com>
2022-07-25 14:07:56 +0200

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+[!-d "./venv/" ] && ./venv/Scripts/python.exe -m pip install -r -/req.txt
+./venv/Scripts/python.exe PC_bridge/manage.py runserver
+
+# installer()
+# {
+#     echo "instaling enviroment..."
+#     py -m venv ./venv
+#     echo "installing libraries..."
+#     ./venv/Scripts/python.exe -m pip install -r -/req.txt
+#     runner
+# }
+
+# runner()
+# {
+#     echo "starting programm..."
+#     ./venv/Scripts/python.exe PC_bridge/manage.py runserver
+# }
+echo "done"
