68003b9469a58a2c99a632c9e1a6eef5b50f0188
marilynias <98028585+marilynias@users.noreply.github.com>
2022-07-25 14:50:48 +0200

    allowed host add pc when db is empty

--- PC_bridge/PC_bridge/settings.py
+++ PC_bridge/PC_bridge/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-b)l!sep&3i82115z3&#$160@o(smm8x9z+m7%@^xjw+(onu+$q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.100.119']
+ALLOWED_HOSTS = ['192.168.100.119', '127.0.0.1']
 
 
 # Application definition
