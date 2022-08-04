e247659396fa5fbd8d00cdb57eb7088d1050061c
marilynias <98028585+marilynias@users.noreply.github.com>
2022-08-04 13:55:40 +0200

    root no longer needed

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 pip install -r req.txt
 python3 PC_bridge/manage.py makemigrations
 python3 PC_bridge/manage.py migrate
-sudo python3 PC_bridge/manage.py runserver 0.0.0.0:8000
+python3 PC_bridge/manage.py runserver 0.0.0.0:8000
