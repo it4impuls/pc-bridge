cbcc0eebda1c6b4b17b9b24650037ad8e102b3d1
marilynias <98028585+marilynias@users.noreply.github.com>
2022-08-08 11:26:15 +0200

    git pull on update seperate run file

---
+++ runServer.sh
@@ -0,0 +1,1 @@
+python3 PC_bridge/manage.py runserver 0.0.0.0:8000
--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git pull
 pip install -r req.txt
 python3 PC_bridge/manage.py makemigrations
 python3 PC_bridge/manage.py migrate
